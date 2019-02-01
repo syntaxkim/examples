66af282dfbdf144b13e78338b9dcdeb947ecae7d
syntaxkim <lovems0219@gmail.com>
2019-02-01 15:14:14 +0900

    directory consolidated

--- python/django_rango/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# this file tells the server how to run Django, and we use waitress for this example, at least to deploy to Heroku
-waitress-serve --port=$PORT latest/rango.wsgi:application
